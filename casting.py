db47aab166bdba54baa711b283b943d196918189
zulfany008 <adohdino6@gmail.com>
2025-03-08 12:05:21 +0700

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,10 @@
+# input
+pensil = int(input('jumlah produk pensil : '))
+penghapus = int(input('jumlah produk penghapus : '))
+
+# proses 
+total = pensil + penghapus 
+
+# output 
+print('hasil penjumlahan :',total)
+print('-----------------------')
