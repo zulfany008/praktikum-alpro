ebb632f8b3b0c7f2aec3197da8cff7ea4fe82900
zulfany008 <adohdino6@gmail.com>
2025-03-08 12:14:49 +0700

    Create typedata.py

---
+++ typedata.py
@@ -0,0 +1,22 @@
+# string
+nama_game = 'Mobile legends'
+# integer
+jumlah_player = 5
+# float
+rata_rata_winstreak = 10
+# list
+nama = ['fany','paundra','skendy','zaki','lukman']
+# tuple
+rank = ('mythic','epic','legends',)
+# set
+hero_favorit = {'claude','hayabusa','sun','tigreal','nana'}
+# dictionary
+data_lengkapnya = {'nama': 'fany','usia':'17'}
+# menampilkan tipe data
+print('data1 tipe datanya :',type(nama_game))
+print('data2 tipe datanya :',type(jumlah_player))
+print('data3 tipe datanya :',type(rata_rata_winstreak))
+print('data4 tipe datanya :',type(nama))
+print('data5 tipe datanya :',type(rank))
+print('data6 tipe datanya :',type(hero_favorit))
+print('data7 tipe datanya :',type(data_lengkapnya))
