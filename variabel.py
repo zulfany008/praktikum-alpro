9dbfb8086e428ca3c991bbeca1b1667016594674
zulfany008 <adohdino6@gmail.com>
2025-03-08 11:57:23 +0700

    Create variabel.py

---
+++ variabel.py
@@ -0,0 +1,9 @@
+# tugas Variable 
+# menghitung luas persegi panjang 
+# Input panjang dan lebar 
+panjang = 12
+Lebar = 8
+# hitung luas
+Luas = panjang * Lebar 
+# Tampilkan hasil
+print("Luas persegi panjang adalah:")
